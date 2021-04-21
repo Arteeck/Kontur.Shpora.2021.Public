7141629160f43b73ffb6bfe3322fbbe7980f27b0
Artemy Gordon <artemy.gordon@gmail.com>
2021-04-21 23:43:53 +0500

    Added ReaderWriterLock

---
+++ ReaderWriterLock/RwLock.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace ReaderWriterLock
+{
+    public class RwLock : IRwLock
+    {
+        private readonly EventWaitHandle wh = new AutoResetEvent(true);
+        private int readers = 0;
+        private readonly object locker = new object();
+
+        public void ReadLocked(Action action)
+        {
+            lock (locker)
+            {
+                Interlocked.Increment(ref readers);
+            }
+
+            action();
+            Interlocked.Decrement(ref readers);
+            wh.Set();
+        }
+
+        public void WriteLocked(Action action)
+        {
+            lock (locker)
+            {
+                while (readers != 0)
+                {
+                    wh.WaitOne();
+                }
+
+                action();
+            }
+        }
+    }
+}
